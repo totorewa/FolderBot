e640059756f579ec50e09d0aa113c02e24c07c12
DesktopFolder <desktopfolder@users.noreply.github.com>
2022-06-12 12:53:16 -0400

    Got our spotify dependency compiling... I think.

--- deps/spotify-rs/src/connector.rs
+++ deps/spotify-rs/src/connector.rs
@@ -1,9 +1,7 @@
-use std::io::Read;
-use std::sync::Mutex;
-use std::net::TcpListener;
-use reqwest::{self, Client};
-use reqwest::header::{Origin, Referer, UserAgent};
 use json::{self, JsonValue};
+use reqwest::header::{ORIGIN, REFERER, USER_AGENT};
+use reqwest::{self, Client};
+use std::net::TcpListener;
 use time;
 
 // Headers
@@ -51,7 +49,7 @@ pub enum InternalSpotifyError {
 /// The `SpotifyConnector` struct.
 pub struct SpotifyConnector {
     /// The Reqwest client.
-    client: Mutex<Client>,
+    client: Client,
     /// The Spotify OAuth token.
     oauth_token: String,
     /// The Spotify CSRF token.
@@ -64,32 +62,29 @@ pub struct SpotifyConnector {
 impl SpotifyConnector {
     /// Constructs a new `SpotifyConnector`.
     /// Retrieves the OAuth and CSRF tokens in the process.
-    pub fn connect_new() -> Result<SpotifyConnector> {
+    pub async fn connect_new() -> Result<SpotifyConnector> {
         // Create the reqwest client.
-        let client = match Client::new() {
-            Ok(client) => client,
-            Err(error) => return Err(InternalSpotifyError::ReqwestError(error)),
-        };
+        let client = Client::new();
         // Create the connector.
         let mut connector = SpotifyConnector {
-            client: Mutex::new(client),
+            client: client,
             oauth_token: String::default(),
             csrf_token: String::default(),
             port: 0, // will be populated later
         };
-        connector.update_port();
+        connector.update_port().await;
         // Connect to SpotifyWebHelper and start Spotify.
-        if let Err(error) = connector.start_spotify () {
+        if let Err(error) = connector.start_spotify().await {
             // The connection failed, error out.
-             return Err(error);
+            return Err(error);
         }
         // Fetch the OAuth token.
-        connector.oauth_token = match connector.fetch_oauth_token() {
+        connector.oauth_token = match connector.fetch_oauth_token().await {
             Ok(result) => result,
             Err(error) => return Err(error),
         };
         // Fetch the CSRF token.
-        connector.csrf_token = match connector.fetch_csrf_token() {
+        connector.csrf_token = match connector.fetch_csrf_token().await {
             Ok(result) => result,
             Err(error) => return Err(error),
         };
@@ -97,28 +92,31 @@ impl SpotifyConnector {
         Ok(connector)
     }
     /// Updates the local Spotify port.
-    fn update_port(&mut self) {
-    	for port in PORT_START..PORT_END {
-    		if TcpListener::bind(("127.0.0.1", port)).is_err() {
-    			self.port = port as i32;
-    			return;
-    		}
-    	}
+    async fn update_port(&mut self) {
+        for port in PORT_START..PORT_END {
+            if TcpListener::bind(("127.0.0.1", port)).is_err() {
+                self.port = port as i32;
+                return;
+            }
+        }
     }
     /// Constructs the local Spotify url.
     fn get_local_url(&self) -> String {
         format!("{}:{}", URL_LOCAL, self.port)
     }
     /// Attempts to start the Spotify client.
-    fn start_spotify(&self) -> Result<bool> {
-        match self.query(&self.get_local_url(), REQUEST_OPEN, false, false, None) {
+    async fn start_spotify(&self) -> Result<bool> {
+        match self
+            .query(&self.get_local_url(), REQUEST_OPEN, false, false, None)
+            .await
+        {
             Ok(result) => Ok(result["running"] == true),
             Err(error) => Err(error),
         }
     }
     /// Fetches the OAuth token from Spotify.
-    fn fetch_oauth_token(&self) -> Result<String> {
-        let json = match self.query(URL_TOKEN, "", false, false, None) {
+    async fn fetch_oauth_token(&self) -> Result<String> {
+        let json = match self.query(URL_TOKEN, "", false, false, None).await {
             Ok(result) => result,
             Err(error) => return Err(error),
         };
@@ -128,8 +126,11 @@ impl SpotifyConnector {
         }
     }
     /// Fetches the CSRF token from Spotify.
-    fn fetch_csrf_token(&self) -> Result<String> {
-        let json = match self.query(&self.get_local_url(), REQUEST_CSRF, false, false, None) {
+    async fn fetch_csrf_token(&self) -> Result<String> {
+        let json = match self
+            .query(&self.get_local_url(), REQUEST_CSRF, false, false, None)
+            .await
+        {
             Ok(result) => result,
             Err(error) => return Err(error),
         };
@@ -139,28 +140,46 @@ impl SpotifyConnector {
         }
     }
     /// Fetches the current status from Spotify.
-    pub fn fetch_status_json(&self) -> Result<JsonValue> {
+    pub async fn fetch_status_json(&self) -> Result<JsonValue> {
         self.query(&self.get_local_url(), REQUEST_STATUS, true, true, None)
+            .await
     }
     /// Requests a track to be played.
-    pub fn request_play(&self, track: String) -> bool {
+    pub async fn request_play(&self, track: String) -> bool {
         let params = vec![format!("uri={0}", track)];
-        self.query(&self.get_local_url(), REQUEST_PLAY, true, true, Some(params)).is_ok()
+        self.query(
+            &self.get_local_url(),
+            REQUEST_PLAY,
+            true,
+            true,
+            Some(params),
+        )
+        .await
+        .is_ok()
     }
     /// Requests the currently playing track to be paused or resumed.
-    pub fn request_pause(&self, pause: bool) -> bool {
+    pub async fn request_pause(&self, pause: bool) -> bool {
         let params = vec![format!("pause={}", pause)];
-        self.query(&self.get_local_url(), REQUEST_PAUSE, true, true, Some(params)).is_ok()
+        self.query(
+            &self.get_local_url(),
+            REQUEST_PAUSE,
+            true,
+            true,
+            Some(params),
+        )
+        .await
+        .is_ok()
     }
     /// Queries the specified base url with the specified query.
     /// Optionally includes the OAuth and/or CSRF token in the query.
-    fn query(&self,
-             base: &str,
-             query: &str,
-             with_oauth: bool,
-             with_csrf: bool,
-             params: Option<Vec<String>>)
-             -> Result<JsonValue> {
+    async fn query(
+        &self,
+        base: &str,
+        query: &str,
+        with_oauth: bool,
+        with_csrf: bool,
+        params: Option<Vec<String>>,
+    ) -> Result<JsonValue> {
         let timestamp = time::now_utc().to_timespec().sec;
         let arguments = {
             let mut arguments = String::new();
@@ -184,21 +203,24 @@ impl SpotifyConnector {
         };
         let url = format!("{}/{}{}", base, query, arguments);
         let response = {
-            let mut content = String::new();
-            let mut resp = match self.client
-                .lock()
-                .unwrap()
+            let resp = match self
+                .client
                 .get::<&str>(url.as_ref())
-                .header(UserAgent(HEADER_UA.into()))
-                .header(Origin::new(HEADER_ORIGIN_SCHEME, HEADER_ORIGIN_HOST, None))
-                .header(Referer(format!("{}/{}", URL_EMBED, REFERAL_TRACK)))
-                .send() {
+                .header(USER_AGENT, format!("{}", HEADER_UA))
+                .header(
+                    ORIGIN,
+                    format!("{}://{}", HEADER_ORIGIN_SCHEME, HEADER_ORIGIN_HOST),
+                )
+                .header(REFERER, format!("{}/{}", URL_EMBED, REFERAL_TRACK))
+                .send()
+                .await
+            {
                 Ok(result) => result,
                 Err(error) => return Err(InternalSpotifyError::ReqwestError(error)),
             };
-            match resp.read_to_string(&mut content) {
-                Ok(_) => content,
-                Err(error) => return Err(InternalSpotifyError::IOError(error)),
+            match resp.text().await {
+                Ok(content) => content,
+                Err(error) => return Err(InternalSpotifyError::ReqwestError(error)),
             }
         };
         match json::parse(response.as_ref()) {
--- deps/spotify-rs/src/lib.rs
+++ deps/spotify-rs/src/lib.rs
@@ -18,12 +18,10 @@ mod connector;
 pub mod status;
 
 // Imports
-use std::thread::{self, JoinHandle};
-use std::time::Duration;
 #[cfg(windows)]
 use windows_process::WindowsProcess;
 use connector::{SpotifyConnector, InternalSpotifyError};
-use status::{SpotifyStatus, SpotifyStatusChange};
+use status::{SpotifyStatus};
 
 /// The `Result` type used in this crate.
 type Result<T> = std::result::Result<T, SpotifyError>;
@@ -46,8 +44,8 @@ pub struct Spotify {
 }
 
 /// Fetches the current status from Spotify.
-fn get_status(connector: &SpotifyConnector) -> Result<SpotifyStatus> {
-    match connector.fetch_status_json() {
+async fn get_status(connector: &SpotifyConnector) -> Result<SpotifyStatus> {
+    match connector.fetch_status_json().await {
         Ok(result) => Ok(SpotifyStatus::from(result)),
         Err(error) => Err(SpotifyError::InternalError(error)),
     }
@@ -57,7 +55,7 @@ fn get_status(connector: &SpotifyConnector) -> Result<SpotifyStatus> {
 impl Spotify {
     /// Connects to the local Spotify client.
     #[cfg(windows)]
-    pub fn connect() -> Result<Spotify> {
+    pub async fn connect() -> Result<Spotify> {
         // TODO:
         // At some point, the connector should automatically
         // open Spotify in the case  that Spotify is closed.
@@ -67,64 +65,26 @@ impl Spotify {
         if !Spotify::spotify_webhelper_alive() {
             return Err(SpotifyError::WebHelperNotRunning);
         }
-        Spotify::new_unchecked()
+        Spotify::new_unchecked().await
     }
     /// Connects to the local Spotify client.
     #[cfg(not(windows))]
-    pub fn connect() -> Result<Spotify> {
-        Spotify::new_unchecked()
+    pub async fn connect() -> Result<Spotify> {
+        Spotify::new_unchecked().await
     }
     /// Constructs a new `self::Result<Spotify>`.
-    fn new_unchecked() -> Result<Spotify> {
-        match SpotifyConnector::connect_new() {
+    async fn new_unchecked() -> Result<Spotify> {
+        match SpotifyConnector::connect_new().await {
             Ok(result) => Ok(Spotify { connector: result }),
             Err(error) => Err(SpotifyError::InternalError(error)),
         }
     }
-    /// Moves `self` to a new thread and begins polling the
-    /// client status. Sends the updated status to the specified
-    /// closure, together with information of which fields had changed
-    /// since the last update. Returns the `JoinHandle` of the new thread.
-    pub fn poll<'a, F: 'static>(self, f: F) -> JoinHandle<()>
-        where F: Fn(&Spotify, SpotifyStatus, SpotifyStatusChange) -> bool,
-              F: std::marker::Send
-    {
-        thread::spawn(move || {
-            let sleep_time = Duration::from_millis(250);
-            let mut last: Option<SpotifyStatus> = None;
-            let mut curr: Option<SpotifyStatus>;
-            let mut first = true;
-            loop {
-                curr = get_status(&self.connector).ok();
-                {
-                    let last = last.clone();
-                    if first && curr.is_some() {
-                        let curr = curr.clone().unwrap();
-                        if !f(&self, curr.clone(), SpotifyStatusChange::new_true()) {
-                            break;
-                        }
-                        first = false;
-                    } else if !first && curr.is_some() && last.is_some() {
-                        let curr = curr.clone().unwrap();
-                        let last = last.unwrap();
-                        if !f(&self, curr.clone(), SpotifyStatusChange::from((curr, last))) {
-                            break;
-                        }
-                    }
-                }
-                if curr.is_some() {
-                    last = curr.clone();
-                }
-                thread::sleep(sleep_time);
-            }
-        })
-    }
     /// Fetches the current status from the client.
-    pub fn status(&self) -> Result<SpotifyStatus> {
-        get_status(&self.connector)
+    pub async fn status(&self) -> Result<SpotifyStatus> {
+        get_status(&self.connector).await
     }
     /// Plays a track.
-    pub fn play(&self, track: String) -> bool {
+    pub async fn play(&self, track: String) -> bool {
         // Try to fix broken track URIs
         // In: https://open.spotify.com/track/1pGZIV8olkbRMjyHWoEXyt
         // In: open.spotify.com/track/1pGZIV8olkbRMjyHWoEXyt
@@ -134,10 +94,10 @@ impl Spotify {
         let track: String = {
             let track = track
                 .replace("https://", "http://") // https -> http
-                .trim_left_matches("http://") // get rid of protocol
-                .trim_left_matches("open.spotify.com") // get rid of domain name
+                .trim_start_matches("http://") // get rid of protocol
+                .trim_start_matches("open.spotify.com") // get rid of domain name
                 .replace("/", ":") // turn all / into :
-                .trim_left_matches(":") // get rid of : at the beginning
+                .trim_start_matches(":") // get rid of : at the beginning
                 .to_owned();
             if track.starts_with("spotify:") {
                 track
@@ -146,17 +106,17 @@ impl Spotify {
             }
         };
         // Play the track
-        self.connector.request_play(track)
+        self.connector.request_play(track).await
     }
     /// Pauses the currently playing track.
     /// Has no effect if the track is already paused.
-    pub fn pause(&self) -> bool {
-        self.connector.request_pause(true)
+    pub async fn pause(&self) -> bool {
+        self.connector.request_pause(true).await
     }
     /// Resumes the currently paused track.
     /// Has no effect if the track is already playing.
-    pub fn resume(&self) -> bool {
-        self.connector.request_pause(false)
+    pub async fn resume(&self) -> bool {
+        self.connector.request_pause(false).await
     }
     /// Tests whether the SpotifyWebHelper process is running.
     #[cfg(windows)]
